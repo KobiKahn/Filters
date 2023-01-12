a5a03321246f5a5590db9804f15442fd43930a67
kobi <kobikahn@gmail.com>
2023-01-12 12:10:45 -0500

    FINISHED FILTERS :)

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import pandas
 import numpy as np
 X = [1 , 4 , 5 , 6, -2 , 9 , 3 , 4 , 0 , 2]
-def mov_avg_fil(f_list, f_len):
-    output = []
-    for i in range(len(f_list)):
-        if i <= len(f_list) - f_len:
-            min_list = []
-            for j in range(f_len):
-                min_list.append(f_list[i + j])
-            output.append(sum(min_list)/f_len)
-
-    return(output)
-
-a_mov_fil = mov_avg_fil(X, 4)
-# print(a_mov_fil)
+W = [1, 1, 1, 1]
 
 X2 = [4 , 7, 8, 2 , 10 , 23 , 9 , 5]
-W = [1 , 2 , 3 , 2 , 1]
+W2 = [1 , 2 , 3 , 2 , 1]
+
+X3 = [4 , 7, 8, 2 , 10 , 23 , 9 , 5]
+W3 = [.5, .5]
 
 def weight_avg_fil(f_list, f_len, w_list):
     output = []
@@ -34,6 +25,29 @@ def weight_avg_fil(f_list, f_len, w_list):
 
     return(output)
 
-w_mov_fil = weight_avg_fil(X2, 5, W)
-print(w_mov_fil)
+w_mov_fil1 = weight_avg_fil(X, 4, W)
+w_mov_fil2 = weight_avg_fil(X2, 5, W2)
+# print(w_mov_fil1)
+# print(w_mov_fil2)
+
+def fade_avg_fil(f_list, w_list):
+    output = []
+    w1, w2 = w_list[0], w_list[1]
+    if sum(w_list) == 1:
+        for i in range(len(f_list)):
+            if i == 0:
+                output.append(f_list[i])
+            else:
+                output.append( (w1 * output[i-1]) + (w2 * f_list[i]) )
+
+    return(output)
+
+f_mov_fil = fade_avg_fil(X3, W3)
+# print(f_mov_fil)
+
+
+
+
+
+
 
